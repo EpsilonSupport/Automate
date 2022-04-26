39ee35a5a6c59da6b83b2abd3082db2533b09dca
EpsilonSupport <63876888+EpsilonSupport@users.noreply.github.com>
2022-04-26 15:48:38 -0400

    Add files via upload

---
+++ Remove-Teams.ps1
@@ -0,0 +1,15 @@
+﻿Get-WmiObject -Class Win32_Product | Where-Object {$_.IdentifyingNumber -eq "{39AF0813-FA7B-4860-ADBE-93B9B214B914}"} | Remove-WmiObject
+
+$TeamsUsersExclusion = ((Get-ChildItem "C:\Users" | Sort-Object LastWriteTime -Descending)[0]).name
+$TeamsUsers = Get-ChildItem -Path "$($ENV:SystemDrive)\Users" -Exclude $TeamsUsersExclusion
+
+$TeamsUsers | ForEach-Object {
+    Try {
+        if (Test-Path "$($ENV:SystemDrive)\Users\$($_.Name)\AppData\Local\Microsoft\Teams") {
+            Remove-Item –Path "$($ENV:SystemDrive)\Users\$($_.Name)\AppData\Local\Microsoft\Teams" -Recurse -Force -ErrorAction Ignore
+        }
+    } Catch {
+        Out-Null
+    }
+}
+
